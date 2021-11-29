c6e56ef47f49491cb6e9a9575809088676cd3f29
Marcel Costa <marcel.costa@bancointer.com.br>
2021-11-29 09:53:23 -0300

    Code for client streaming

--- src/main/java/com/tdc/heroes/client/HeroesClient.java
+++ src/main/java/com/tdc/heroes/client/HeroesClient.java
@@ -3,54 +3,53 @@ package com.tdc.heroes.client;
 import com.tdc.heroes.CreateHeroRequest;
 import com.tdc.heroes.CreateHeroResponse;
 import com.tdc.heroes.HeroesServiceGrpc;
-import com.tdc.heroes.ListHeroesRequest;
-import com.tdc.heroes.ListHeroesResponse;
 import com.tdc.heroes.Skill;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
+
+import java.util.concurrent.CountDownLatch;
 
 public class HeroesClient {
 
     private static final String HOST = "localhost";
     private static final int PORT = 50051;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("HeroesClient has started");
 
         ManagedChannel channel = openChannel();
 
-        // unary request (blocking stub)
-            HeroesServiceGrpc.HeroesServiceBlockingStub heroesStub = HeroesServiceGrpc.newBlockingStub(channel);
+        // client streaming (new stub - async)
+        HeroesServiceGrpc.HeroesServiceStub heroesStub = HeroesServiceGrpc.newStub(channel);
 
-        // create a new hero
-        CreateHeroRequest createBatmanRequest = CreateHeroRequest.newBuilder()
-                .addSkills(Skill.newBuilder().setStrength("martial arts").build())
-                .addSkills(Skill.newBuilder().setStrength("wisdom").build())
-                .setNickname("Batman")
-                .build();
 
         // invoke gRPC service [createHero]
-        CreateHeroResponse createBatmanResponse = heroesStub.createHero(createBatmanRequest);
-        System.out.println("New hero: " + createBatmanResponse);
+        CountDownLatch latch = new CountDownLatch(1);
+        StreamObserver<CreateHeroRequest> createBatmanObserver = heroesStub.createHero(new CreateHeroResponseStreamObserver(latch));
 
-        // create a new hero
-        CreateHeroRequest createFlashRequest = CreateHeroRequest.newBuilder()
-                .addSkills(Skill.newBuilder().setStrength("super speed").build())
-                .addSkills(Skill.newBuilder().setStrength("time travel").build())
-                .setNickname("Flash")
-                .build();
+        createBatmanObserver.onNext(
+            createHero("Batman", "wealth")
+        );
 
-        // invoke gRPC service [createHero]
-        CreateHeroResponse createFlashResponse = heroesStub.createHero(createFlashRequest);
-        System.out.println("New hero: " + createFlashResponse);
+        createBatmanObserver.onNext(
+                createHero("Batman", "martial arts")
+        );
+
+        createBatmanObserver.onNext(
+                createHero("Batman", "weapon skills")
+        );
+
+        createBatmanObserver.onNext(
+                createHero("Batman", "wisdom")
+        );
+
+        createBatmanObserver.onCompleted();
+
+        latch.await();
 
         // **** **** ****
-
-        // invoke gRPC service [listHeroes]
-        ListHeroesResponse listHeroesResponse =
-                heroesStub.listHeroes(ListHeroesRequest.newBuilder().build());
-
-        System.out.println("List of heroes: " + listHeroesResponse.getHeroesList());
 
         channel.shutdown();
     }
@@ -59,6 +58,39 @@ public class HeroesClient {
         return  ManagedChannelBuilder
                 .forAddress(HOST, PORT)
                 .usePlaintext()
+                .build();
+    }
+
+    private static class CreateHeroResponseStreamObserver implements StreamObserver<CreateHeroResponse> {
+
+        private CountDownLatch latch;
+
+        CreateHeroResponseStreamObserver(CountDownLatch latch) {
+            this.latch = latch;
+        }
+
+        @Override
+        public void onNext(CreateHeroResponse heroCreated) {
+            System.out.println(heroCreated);
+        }
+
+        @Override
+        public void onError(Throwable t) {
+            Status error = Status.fromCode(Status.Code.UNKNOWN);
+            System.out.println(error.withDescription("An error has happened on client CreateHeroRequest"));
+            throw new RuntimeException(error.asException());
+        }
+
+        @Override
+        public void onCompleted() {
+            this.latch.countDown();
+        }
+    }
+
+    private static CreateHeroRequest createHero(String nickname, String skill) {
+        return CreateHeroRequest.newBuilder()
+                .addSkills(Skill.newBuilder().setStrength(skill).build())
+                .setNickname(nickname)
                 .build();
     }
 }
--- src/main/java/com/tdc/heroes/server/HeroesServiceImpl.java
+++ src/main/java/com/tdc/heroes/server/HeroesServiceImpl.java
@@ -8,35 +8,70 @@ import com.tdc.heroes.ListHeroesRequest;
 import com.tdc.heroes.ListHeroesResponse;
 import io.grpc.stub.StreamObserver;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HeroesServiceImpl extends HeroesServiceGrpc.HeroesServiceImplBase {
 
-    private List<Hero> heroes = new ArrayList<>();
+    private Map<String, Hero> heroes = new HashMap<>();
 
     @Override
-    public void createHero(CreateHeroRequest request, StreamObserver<CreateHeroResponse> responseObserver) {
-        Hero newHero = Hero.newBuilder()
-                .setNickname(request.getNickname())
-                .addAllSkills(request.getSkillsList())
-                .build();
+    public StreamObserver<CreateHeroRequest> createHero(StreamObserver<CreateHeroResponse> responseObserver) {
+        return new StreamObserver<>() {
+            @Override
+            public void onNext(CreateHeroRequest createHeroRequest) {
+                // if hero does exist then include skill only
+                if (heroes.containsKey(createHeroRequest.getNickname())) {
+                    Hero heroExisted = heroes.get(createHeroRequest.getNickname());
 
-        CreateHeroResponse heroResponse = CreateHeroResponse.newBuilder()
-                .setHero(newHero)
-                .build();
+                    Hero hero = Hero.newBuilder()
+                            .setNickname(createHeroRequest.getNickname())
+                            .addAllSkills(createHeroRequest.getSkillsList())
+                            .addAllSkills(heroExisted.getSkillsList())
+                            .build();
 
-        System.out.println("Hero created: " + newHero);
-        this.heroes.add(newHero);
+                    heroes.replace(createHeroRequest.getNickname(), hero);
 
-        responseObserver.onNext(heroResponse);
-        responseObserver.onCompleted();
+                    System.out.println(createHeroRequest.getNickname() + " skills updated");
+
+                // new hero
+                } else {
+                    heroes.put(createHeroRequest.getNickname()
+                            , Hero.newBuilder()
+                                    .setNickname(createHeroRequest.getNickname())
+                                    .addAllSkills(createHeroRequest.getSkillsList())
+                                    .build());
+
+                    if (heroes.size() > 1) {
+                        throw new RuntimeException("Hero should be unique on the request");
+                    }
+
+                    System.out.println("New hero created: " + createHeroRequest.getNickname());
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println("An error has happened: " + t);
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onNext(
+                       CreateHeroResponse.newBuilder()
+                               .setHero(heroes.values().stream().findFirst().get())
+                               .build()
+                );
+
+                responseObserver.onCompleted();
+            }
+        };
     }
 
     @Override
     public void listHeroes(ListHeroesRequest request, StreamObserver<ListHeroesResponse> responseObserver) {
         ListHeroesResponse response = ListHeroesResponse.newBuilder()
-                .addAllHeroes(this.heroes)
+                .addAllHeroes(this.heroes.values())
                 .build();
 
         System.out.println("List of heroes: "  + this.heroes);
